e1875ef8878384435aae49c170be35d006300a22
Warren MacEvoy <wmacevoy@gmail.com>
2020-02-04 15:03:18 -0700

    bouncy cat

--- docs/js/cat.js
+++ docs/js/cat.js
@@ -1,12 +1,31 @@
-function seeCat() {
-   let cat = document.getElementById("cat")
-   cat.style = "display: block"
+let catX = 0;
+let catY = 0;
+let catScore = 0;
+
+function logdiv(message) {
+    let div = document.getElementById("log");
+    div.innerHTML += "<br>" + message;
 }
 
-function hideCat() {
-    let cat = document.getElementById("cat")
-    cat.style = "display: none" 
+function clickCat() {
+    catScore = catScore + 1;
 }
 
-hideCat()
-setTimeout(() => { alert('kitty!'); seeCat() }, 4000)
+function moveCat() {
+   let cat = document.getElementById("cat");
+   catX = Math.random()*400;
+   catY = Math.random()*400;
+   let newStyle = "left: " + catX + "px; top: " + catY + "px;"
+   console.log(newStyle);
+   cat.style = newStyle;
+   logdiv("🤐: " + newStyle)
+   catScore = catScore - 1;
+
+}
+
+function updateScore() {
+    let score = document.getElementById("score");
+    score.innerHTML = catScore;
+}
+
+setInterval(() => { moveCat(); updateScore() }, 1000)
