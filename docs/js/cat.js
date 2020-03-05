3595e9babd2919bdb8ee9b809b4018d3aba098d0
Warren MacEvoy <wmacevoy@gmail.com>
2020-03-05 14:01:18 -0700

    cats

--- docs/js/cat.js
+++ docs/js/cat.js
@@ -1,3 +1,35 @@
+class Cat {
+    constructor(id,originX,originY,width,height) {
+        this.id = id;
+        this.originX = originX;
+        this.originY = originY;
+        this.width = width;
+        this.height = height;
+        this.x = 0;
+        this.y = 0;
+        this.score = 0;
+        setInterval(()=>{ this.move(); this.updateScore() }, 1000)
+    }
+
+    click() {
+        this.score = this.score + 1;
+    }
+    
+    move() {
+        let cat = document.getElementById(this.id);
+        this.x = this.originX + Math.random()*this.width;
+        this.y = this.originY + Math.random()*this.height;
+        let newStyle = "left: " + this.x + "px; top: " + this.y + "px;"
+        cat.style = newStyle;
+    }
+
+    updateScore() {
+        let score = document.getElementById(this.id + "-score");
+        score.innerHTML = this.score;
+    }
+
+}
+
 let catX = 0;
 let catY = 0;
 let catScore = 0;
